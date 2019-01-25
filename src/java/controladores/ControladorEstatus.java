4c7de4297e072131286d663d222c6be02ea5dda5
marlongomez <marlonegomezh@gmail.com>
2019-01-25 09:55:03 -0500

    elaborando exportacion en excel

--- src/java/controladores/ControladorEstatus.java
+++ src/java/controladores/ControladorEstatus.java
@@ -5,15 +5,31 @@
 package controladores;
 
 import Modelo.GestionStatus;
+import static Modelo.conectaMysql.getConnection;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javabeans.Status;
 import javabeans.Usuario;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 /**
  *
@@ -84,8 +100,11 @@ public class ControladorEstatus extends ControladorBase {
         rd.forward(request,response);
     }
     
-    public void reporte(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        Map param = new HashMap();
-        generarReporte("ReporteStatus.jasper", param, request, response);
+    public void reporte(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        /*Map param = new HashMap();
+        generarReporte("ReporteStatus.jasper", param, request, response);*/
+        //generarReporteExcel("ReporteStatus.jasper", param, request, response);
+
+      
     }
 }
